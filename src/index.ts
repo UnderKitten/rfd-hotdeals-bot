88d208bee8650bc8a64a3754bc8a5bc2cdc6984f
Maxim Churzin <mchurzin@outlook.com>
2026-05-15 11:29:19 -0400

    Add initial rfd fetch request

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("RFD deal bot started");
+import axios from "axios";
+
+
+async function main() {
+  const response = await axios.get(
+    "https://forums.redflagdeals.com/hot-deals-f9/",
+  );
+
+  console.log(response.status);
+  console.log(response.data.substring(0, 500));
+}
+
+main();
